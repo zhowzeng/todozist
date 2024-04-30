59f9dead345f4e9e48eda30aabf34c8e344ab6ee
zhowzeng <zenglihao106@gmail.com>
2024-04-30 21:40:16 +0800

    refactor: introduce clap to add, remove, done and list dotos

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,30 @@
+use clap::{Parser, ValueEnum};
 use serde::{Deserialize, Serialize};
 use std::fs;
 
+#[derive(ValueEnum, Clone, Debug)]
+enum Action {
+    List,
+    Add,
+    Done,
+    Remove,
+}
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    // #[arg(short, long)]
+    #[clap(value_enum)]
+    action: Action,
+    #[arg(short, long)]
+    id: u8,
+    #[arg(short, long)]
+    description: String,
+}
+
 #[derive(Serialize, Deserialize, Debug)]
 struct Todo {
+    id: u8,
     done: bool,
     description: String,
 }
@@ -18,15 +40,17 @@ impl Database {
         println!("owner: {}", self.owner);
         for todo in self.todo_list.iter() {
             if todo.done {
-                println!("[x] {}", todo.description);
+                println!("[x] {}) {}", todo.id, todo.description);
             } else {
-                println!("[ ] {}", todo.description);
+                println!("[ ] {}) {}", todo.id, todo.description);
             }
         }
     }
 }
 
 fn main() {
+    let args = Args::parse();
+    println!("{:?}", args);
     let mut db = match fs::read_to_string("./todozist.json") {
         Ok(s) => {
             println!("load database");
@@ -42,17 +66,27 @@ fn main() {
             }
         }
     };
-    // let deserialized: Point = serde_json::from_str(&serialized).unwrap();
-    let todo = Todo {
-        done: false,
-        description: String::from("do something"),
-    };
-    let todo2 = Todo {
-        done: true,
-        description: String::from("done something"),
-    };
-    db.todo_list.push(todo);
-    db.todo_list.push(todo2);
+    match args.action {
+        Action::Add => {
+            let new = Todo {
+                id: args.id,
+                done: false,
+                description: args.description,
+            };
+            db.todo_list.push(new);
+        }
+        Action::Remove => {
+            db.todo_list.retain(|todo| todo.id != args.id);
+        }
+        Action::Done => {
+            for todo in db.todo_list.iter_mut() {
+                if todo.id == args.id {
+                    todo.done = true;
+                }
+            }
+        }
+        _ => {}
+    }
     db.display();
     let serialized = serde_json::to_string_pretty(&db).unwrap();
     fs::write("./todozist.json", serialized).expect("fail to write json");
